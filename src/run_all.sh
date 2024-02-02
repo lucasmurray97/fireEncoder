c7169103e7554cf1f814c9db625a4f2b92869feb
lucasmurray97 <37586981+lucasmurray97@users.noreply.github.com>
2024-02-02 18:39:39 -0300

    Testing weight_decay parameter

--- src/run_all.sh
+++ src/run_all.sh
@@ -1,40 +1,11 @@
 #!/bin/bash
+
+python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6 --normalize  --weight_decay 0 --lr1 1e-5 --lr2 1e-5 --lr3 1e-5
+python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6 --normalize --weight_decay 1e-1 --lr1 1e-5 --lr2 1e-5 --lr3 1e-5
+python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6 --normalize --weight_decay 1e-2 --lr1 1e-5 --lr2 1e-5 --lr3 1e-5
+python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6 --normalize --weight_decay 1e-3 --lr1 1e-5 --lr2 1e-5 --lr3 1e-5
+
 python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-5 --lr2 1e-5 --lr3 1e-5
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-5 --lr2 1e-5 --lr3 1e-4
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-5 --lr2 1e-5 --lr3 1e-6
-
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-5 --lr2 1e-4 --lr3 1e-5
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-5 --lr2 1e-4 --lr3 1e-4
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-5 --lr2 1e-4 --lr3 1e-6
-
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-5 --lr2 1e-6 --lr3 1e-5
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-5 --lr2 1e-6 --lr3 1e-4
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-5 --lr2 1e-6 --lr3 1e-6
-
-####
-
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-4 --lr2 1e-5 --lr3 1e-5
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-4 --lr2 1e-5 --lr3 1e-4
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-4 --lr2 1e-5 --lr3 1e-6
-
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-4 --lr2 1e-4 --lr3 1e-5
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-4 --lr2 1e-4 --lr3 1e-4
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-4 --lr2 1e-4 --lr3 1e-6
-
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-4 --lr2 1e-6 --lr3 1e-5
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-4 --lr2 1e-6 --lr3 1e-4
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-4 --lr2 1e-6 --lr3 1e-6
-
-####
-
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-6 --lr2 1e-5 --lr3 1e-5
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-6 --lr2 1e-5 --lr3 1e-4
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-6 --lr2 1e-5 --lr3 1e-6
-
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-6 --lr2 1e-4 --lr3 1e-5
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-6 --lr2 1e-4 --lr3 1e-4
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-6 --lr2 1e-4 --lr3 1e-6
-
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-6 --lr2 1e-6 --lr3 1e-5
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-6 --lr2 1e-6 --lr3 1e-4
-python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 0 --lr1 1e-6 --lr2 1e-6 --lr3 1e-6
+python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 1e-1 --lr1 1e-5 --lr2 1e-5 --lr3 1e-5
+python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 1e-2 --lr1 1e-5 --lr2 1e-5 --lr3 1e-5
+python train_ae.py --latent_dim 256 --epochs 100 --network AE_Reward --temperature 1e6  --weight_decay 1e-3 --lr1 1e-5 --lr2 1e-5 --lr3 1e-5
