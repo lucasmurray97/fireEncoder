c1d7ceef88fa2ddcad096c995e9ffeee60cdb71c
lucasmurray97 <37586981+lucasmurray97@users.noreply.github.com>
2024-03-01 11:31:18 -0300

    Vae ready for testing

--- src/networks/vae.py
+++ src/networks/vae.py
@@ -8,26 +8,30 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 class VAE(nn.Module):
-    def __init__(self, capacity, input_size, latent_dims, sigmoid=False, instance = "homo_2"):
+    def __init__(self, params):
         super(VAE, self).__init__()
         self.name = "VAE"
-        self.instance= instance
-        self.latent_dims = latent_dims
-        self.c = capacity
+        self.instance= params["instance"]
+        self.latent_dims = params["latent_dims"]
+        self.c = params["capacity"]
+        self.input_size = params["input_size"]
         kernel_size = 4
         stride = 2
         padding = 1
-        self.dim_1 = int((input_size - kernel_size + 2*padding)/2 + 1)
+        self.dim_1 = int((self.input_size - kernel_size + 2*padding)/2 + 1)
         self.dim_2 = int((self.dim_1 - kernel_size + 2*padding)/2 + 1)
-        
+        sigmoid = params["sigmoid"]
+        self.is_sigmoid = sigmoid
+        self.lr1 = params["lr1"]
+        self.device  = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         # Encoder layers:
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=self.c, kernel_size=kernel_size, stride=stride, padding=padding) # (64, 10, 10)
         self.conv2 = nn.Conv2d(in_channels=self.c, out_channels=self.c*2, kernel_size=kernel_size, stride=stride, padding=padding) # (128, 5, 5)
-        self.fc_mu = nn.Linear(in_features=latent_dims*(self.dim_2**2), out_features = latent_dims)
-        self.fc_logvar = nn.Linear(in_features=latent_dims*(self.dim_2**2), out_features = latent_dims)
+        self.fc_mu = nn.Linear(in_features=self.latent_dims*(self.dim_2**2), out_features = self.latent_dims)
+        self.fc_logvar = nn.Linear(in_features=self.latent_dims*(self.dim_2**2), out_features = self.latent_dims)
         
         # Decoder layers:
-        self.fc = nn.Linear(in_features=latent_dims, out_features=latent_dims*(self.dim_2**2))
+        self.fc = nn.Linear(in_features=self.latent_dims, out_features=self.latent_dims*(self.dim_2**2))
         self.conv1_ = nn.ConvTranspose2d(in_channels=self.c*2, out_channels=self.c, kernel_size=kernel_size, stride=stride, padding=padding)
         self.conv2_ = nn.ConvTranspose2d(in_channels=self.c, out_channels=1, kernel_size=kernel_size, stride=stride, padding=padding)
 
@@ -36,7 +40,8 @@ class VAE(nn.Module):
         nn.init.kaiming_uniform_(self.conv2.weight, mode='fan_in', nonlinearity='relu')
         nn.init.kaiming_uniform_(self.conv1_.weight, mode='fan_in', nonlinearity='relu')
         nn.init.kaiming_uniform_(self.conv2_.weight, mode='fan_in', nonlinearity='relu')
-
+        # Optimizer:
+        self.optimizer = torch.optim.Adam(self.parameters(), lr = self.lr1)
         # Losses holders:
         self.training_loss = []
         self.validation_loss = []
@@ -52,7 +57,7 @@ class VAE(nn.Module):
         self.val_reconstruction_epoch_loss = 0
         self.divergence_epoch_loss = 0
         self.val_divergence_epoch_loss = 0
-
+        self.variational_beta = 1
         self.last_mu = None
         self.last_logvar = None
         
@@ -71,7 +76,7 @@ class VAE(nn.Module):
         x = torch.sigmoid(self.conv2_(x)) # last layer before output is sigmoid, since we are using BCE as reconstruction loss
         return x
     
-    def forward(self, x):
+    def forward(self, x, r):
         latent_mu, latent_logvar = self.encode(x)
         latent = self.latent_sample(latent_mu, latent_logvar)
         x_recon = self.decode(latent)
@@ -102,7 +107,7 @@ class VAE(nn.Module):
         # (the one we are going to sample from when generating new images)
         # and the distribution estimated by the generator for the given image.
         kldivergence = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-        return recon_loss, kldivergence, recon_loss + variational_beta * kldivergence
+        return recon_loss, kldivergence, recon_loss + self.variational_beta * kldivergence
 
     def loss(self, output, x, r):
         mu, logvar = (self.last_mu, self.last_logvar)
@@ -121,6 +126,12 @@ class VAE(nn.Module):
         self.val_epoch_loss += loss.item()
         return loss
         
+    def step(self):
+        self.optimizer.step()
+    
+    def zero_grad(self):
+        self.optimizer.zero_grad()
+
     def reset_losses(self):
         self.training_loss.append(self.epoch_loss/self.n)
         self.validation_loss.append(self.val_epoch_loss/self.m)
@@ -146,7 +157,7 @@ class VAE(nn.Module):
         plt.xlabel('Epochs')
         plt.ylabel('Loss')
         plt.legend()
-        plt.savefig(f"experiments/{self.instance}/train_stats/{self.name}/loss_sub20x20_latent={self.latent_dims}_capacity={self.c}_{epochs}_sigmoid={self.is_sigmoid}_T1={self.T1}_T2={self.T2}_lr1={self.lr1}_lr2={self.lr2}_lr3={self.lr3}_normalize={self.normalize}_weight_decay={self.weight_decay}.png")
+        plt.savefig(f"experiments/{self.instance}/train_stats/{self.name}/loss_sub20x20_latent={self.latent_dims}_capacity={self.c}_{epochs}_sigmoid={self.is_sigmoid}_lr1={self.lr1}.png")
 
         plt.ion()
         fig = plt.figure()
@@ -155,7 +166,7 @@ class VAE(nn.Module):
         plt.xlabel('Epochs')
         plt.ylabel('Loss')
         plt.legend()
-        plt.savefig(f"experiments/{self.instance}/train_stats/{self.name}/reconstruction_loss_sub20x20_latent={self.latent_dims}_capacity={self.c}_{epochs}_sigmoid={self.is_sigmoid}_T1={self.T1}_T2={self.T2}_lr1={self.lr1}_lr2={self.lr2}_lr3={self.lr3}_normalize={self.normalize}_weight_decay={self.weight_decay}.png")
+        plt.savefig(f"experiments/{self.instance}/train_stats/{self.name}/reconstruction_loss_sub20x20_latent={self.latent_dims}_capacity={self.c}_{epochs}_sigmoid={self.is_sigmoid}_lr1={self.lr1}.png")
 
         plt.ion()
         fig = plt.figure()
@@ -164,7 +175,7 @@ class VAE(nn.Module):
         plt.xlabel('Epochs')
         plt.ylabel('Loss')
         plt.legend()
-        plt.savefig(f"experiments/{self.instance}/train_stats/{self.name}/divergence_loss_sub20x20_latent={self.latent_dims}_capacity={self.c}_{epochs}_sigmoid={self.is_sigmoid}_T1={self.T1}_T2={self.T2}_lr1={self.lr1}_lr2={self.lr2}_lr3={self.lr3}_normalize={self.normalize}_weight_decay={self.weight_decay}.png")
+        plt.savefig(f"experiments/{self.instance}/train_stats/{self.name}/divergence_loss_sub20x20_latent={self.latent_dims}_capacity={self.c}_{epochs}_sigmoid={self.is_sigmoid}_lr1={self.lr1}.png")
 
     def calc_test_loss(self, output, images, r):
         return self.loss(output, images, r)
--- src/run_all.sh
+++ src/run_all.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 ### AE
-python train_ae.py --latent_dim 256 --epochs 100 --network AE --instance hetero_1 --lr1 1e-3
-python train_ae.py --latent_dim 256 --epochs 100 --network AE --instance hetero_1 --lr1 1e-4
-python train_ae.py --latent_dim 256 --epochs 100 --network AE --instance hetero_1 --lr1 1e-5
+python train_ae.py --latent_dim 256 --epochs 100 --network VAE --instance homo_2 --lr1 1e-3
+python train_ae.py --latent_dim 256 --epochs 100 --network VAE --instance homo_2 --lr1 1e-4
+python train_ae.py --latent_dim 256 --epochs 100 --network VAE --instance homo_2 --lr1 1e-5
 
-python train_ae.py --latent_dim 128 --epochs 100 --network AE --instance hetero_1 --lr1 1e-3
-python train_ae.py --latent_dim 128 --epochs 100 --network AE --instance hetero_1 --lr1 1e-4
-python train_ae.py --latent_dim 128 --epochs 100 --network AE --instance hetero_1 --lr1 1e-5
+python train_ae.py --latent_dim 128 --epochs 100 --network VAE --instance homo_2 --lr1 1e-3
+python train_ae.py --latent_dim 128 --epochs 100 --network VAE --instance homo_2 --lr1 1e-4
+python train_ae.py --latent_dim 128 --epochs 100 --network VAE --instance homo_2 --lr1 1e-5
 
-python train_ae.py --latent_dim 64 --epochs 100 --network AE --instance hetero_1 --lr1 1e-3
-python train_ae.py --latent_dim 64 --epochs 100 --network AE --instance hetero_1 --lr1 1e-4
-python train_ae.py --latent_dim 64 --epochs 100 --network AE --instance hetero_1 --lr1 1e-5
+python train_ae.py --latent_dim 64 --epochs 100 --network VAE --instance homo_2 --lr1 1e-3
+python train_ae.py --latent_dim 64 --epochs 100 --network VAE --instance homo_2 --lr1 1e-4
+python train_ae.py --latent_dim 64 --epochs 100 --network VAE --instance homo_2 --lr1 1e-5
 
-
+: '
 ### AE reward
 python train_ae.py --latent_dim 256 --epochs 100 --instance hetero_1 --network AE_Reward --temperature_1 1e3 --temperature_2 1e6 --normalize  --weight_decay 0 --lr1 1e-5 --lr2 1e-5 --lr3 1e-5
 python train_ae.py --latent_dim 256 --epochs 100 --instance hetero_1 --network AE_Reward --temperature_1 1e3 --temperature_2 1e6 --normalize  --weight_decay 0 --lr1 1e-5 --lr2 1e-5 --lr3 1e-4
@@ -312,3 +312,4 @@ python train_ae.py --latent_dim 256 --epochs 100 --instance hetero_1 --network A
 python train_ae.py --latent_dim 256 --epochs 100 --instance hetero_1 --network AE_Reward 1e4 --temperature_2 1e5 --normalize  --weight_decay 1e-5 --lr1 1e-6 --lr2 1e-6 --lr3 1e-4
 python train_ae.py --latent_dim 256 --epochs 100 --instance hetero_1 --network AE_Reward 1e4 --temperature_2 1e5 --normalize  --weight_decay 1e-5 --lr1 1e-6 --lr2 1e-6 --lr3 1e-6
 
+'
