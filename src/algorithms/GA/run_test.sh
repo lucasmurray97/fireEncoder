432b38c706c83afa27b7945a4c2a867508ed9bb5
lucasmurray97 <lucasmurrayh@gmail.com>
2024-07-05 16:32:02 -0400

    added bash script

---
+++ src/algorithms/GA/run_test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 100 --alpha 0.5 --mutation_rate 0.2
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 100 --alpha 0.5 --mutation_rate 0.5
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 100 --alpha 0.5 --mutation_rate 0.8
+
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 200 --alpha 0.5 --mutation_rate 0.2
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 200 --alpha 0.5 --mutation_rate 0.5
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 200 --alpha 0.5 --mutation_rate 0.8
+
+##
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.05 --population_size 100 --alpha 0.5 --mutation_rate 0.2
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.05 --population_size 100 --alpha 0.5 --mutation_rate 0.5
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.05 --population_size 100 --alpha 0.5 --mutation_rate 0.8
+
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.05 --population_size 200 --alpha 0.5 --mutation_rate 0.2
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.05 --population_size 200 --alpha 0.5 --mutation_rate 0.5
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.05 --population_size 200 --alpha 0.5 --mutation_rate 0.8
+
+###
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 100 --alpha 0.8 --mutation_rate 0.2
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 100 --alpha 0.8 --mutation_rate 0.5
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 100 --alpha 0.8 --mutation_rate 0.8
+
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 200 --alpha 0.8 --mutation_rate 0.2
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 200 --alpha 0.8 --mutation_rate 0.5
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 200 --alpha 0.8 --mutation_rate 0.8
+
+##
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.05 --population_size 100 --alpha 0.8 --mutation_rate 0.2
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.05 --population_size 100 --alpha 0.8 --mutation_rate 0.5
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.05 --population_size 100 --alpha 0.8 --mutation_rate 0.8
+
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.05 --population_size 200 --alpha 0.8 --mutation_rate 0.2
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.05 --population_size 200 --alpha 0.8 --mutation_rate 0.5
+python train.py --n_repeats 20 --iters 100 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.05 --population_size 200 --alpha 0.8 --mutation_rate 0.8
+
