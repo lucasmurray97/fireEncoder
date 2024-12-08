02b314dced0fc2d920169c98e4e6d3fcdc0c7fa7
lucasmurray97 <lucasmurrayh@gmail.com>
2024-12-08 10:08:14 +0100

    ready to run experiment

--- src/run_all.sh
+++ src/run_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ### AE
-python train_ae.py --latent_dim 256 --epochs 100 --network CCVAE --instance homo_2 --lr1 1e-5 --lr2 1e-5 --distribution_std 0.1 --sigmoid --variational_beta 0.1 --alpha 1e4 --data_version 1
 python train_ae.py --latent_dim 256 --epochs 100 --network CCVAE --instance homo_2 --lr1 1e-5 --lr2 1e-5 --distribution_std 0.1 --sigmoid --variational_beta 0.1 --alpha 1e5 --data_version 1
-python train_ae.py --latent_dim 256 --epochs 100 --network CCVAE --instance homo_2 --lr1 1e-5 --lr2 1e-5 --distribution_std 0.1 --sigmoid --variational_beta 0.1 --alpha 1e6 --data_version 1
+python train_ae.py --latent_dim 256 --epochs 100 --network CCVAE --instance homo_2 --lr1 1e-5 --lr2 1e-5 --distribution_std 0.2 --sigmoid --variational_beta 0.1 --alpha 1e5 --data_version 1
+python train_ae.py --latent_dim 256 --epochs 100 --network CCVAE --instance homo_2 --lr1 1e-5 --lr2 1e-5 --distribution_std 0.5 --sigmoid --variational_beta 0.1 --alpha 1e5 --data_version 1
 
-python train_ae.py --latent_dim 256 --epochs 100 --network CCVAE --instance homo_2 --lr1 1e-5 --lr2 1e-6 --distribution_std 0.1 --sigmoid --variational_beta 0.1 --alpha 1e4 --data_version 1
 python train_ae.py --latent_dim 256 --epochs 100 --network CCVAE --instance homo_2 --lr1 1e-5 --lr2 1e-6 --distribution_std 0.1 --sigmoid --variational_beta 0.1 --alpha 1e5 --data_version 1
-python train_ae.py --latent_dim 256 --epochs 100 --network CCVAE --instance homo_2 --lr1 1e-5 --lr2 1e-6 --distribution_std 0.1 --sigmoid --variational_beta 0.1 --alpha 1e6 --data_version 1
+python train_ae.py --latent_dim 256 --epochs 100 --network CCVAE --instance homo_2 --lr1 1e-5 --lr2 1e-6 --distribution_std 0.2 --sigmoid --variational_beta 0.1 --alpha 1e5 --data_version 1
+python train_ae.py --latent_dim 256 --epochs 100 --network CCVAE --instance homo_2 --lr1 1e-5 --lr2 1e-6 --distribution_std 0.5 --sigmoid --variational_beta 0.1 --alpha 1e5 --data_version 1
