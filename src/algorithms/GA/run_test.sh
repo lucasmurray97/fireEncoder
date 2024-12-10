030494dc0028d3301d0bfec3746008c47a12a12b
lucasmurray97 <lucasmurrayh@gmail.com>
2024-12-10 12:40:46 +0100

    ready for experiments

--- src/algorithms/GA/run_test.sh
+++ src/algorithms/GA/run_test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-python train.py --n_repeats 8 --iters 40 --algorithm vanilla --variational_beta 0.1 --distribution_std 1. --initial_population 0.005 --population_size 200 --alpha 0.5 --mutation_rate 0.2
-python train.py --n_repeats 8 --iters 40 --algorithm vanilla --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 200 --alpha 0.5 --mutation_rate 0.5
+python train.py --n_repeats 8 --iters 40 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.005 --population_size 200 --alpha 0.5 --mutation_rate 0.2 --net ccvae
+python train.py --n_repeats 8 --iters 40 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 200 --alpha 0.5 --mutation_rate 0.5  --net ccvae
 
-python train.py --n_repeats 8 --iters 40 --algorithm vanilla --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 300 --alpha 0.5 --mutation_rate 0.2
-python train.py --n_repeats 8 --iters 40 --algorithm vanilla --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 300 --alpha 0.5 --mutation_rate 0.5
+python train.py --n_repeats 8 --iters 40 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 300 --alpha 0.5 --mutation_rate 0.2  --net ccvae
+python train.py --n_repeats 8 --iters 40 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 300 --alpha 0.5 --mutation_rate 0.5  --net ccvae
 
 ##
-python train.py --n_repeats 8 --iters 40 --algorithm vanilla --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 200 --alpha 0.8 --mutation_rate 0.2
-python train.py --n_repeats 8 --iters 40 --algorithm vanilla --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 200 --alpha 0.8 --mutation_rate 0.5
+python train.py --n_repeats 8 --iters 40 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 200 --alpha 0.8 --mutation_rate 0.2  --net ccvae
+python train.py --n_repeats 8 --iters 40 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 200 --alpha 0.8 --mutation_rate 0.5  --net ccvae
 
-python train.py --n_repeats 8 --iters 40 --algorithm vanilla --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 300 --alpha 0.8 --mutation_rate 0.2
-python train.py --n_repeats 8 --iters 40 --algorithm vanilla --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 300 --alpha 0.8 --mutation_rate 0.5
+python train.py --n_repeats 8 --iters 40 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 300 --alpha 0.8 --mutation_rate 0.2  --net ccvae
+python train.py --n_repeats 8 --iters 40 --algorithm v1 --variational_beta 0.1 --distribution_std 1. --initial_population 0.01 --population_size 300 --alpha 0.8 --mutation_rate 0.5  --net ccvae
