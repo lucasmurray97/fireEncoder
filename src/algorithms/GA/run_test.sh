420190ab1017f92e19fd9f80ddbe68e54017941a
lucasmurray97 <lucasmurrayh@gmail.com>
2024-12-11 11:52:08 +0100

    Ready for experiment

--- src/algorithms/GA/run_test.sh
+++ src/algorithms/GA/run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python train.py --n_repeats 6 --iters 20 --algorithm v1 --variational_beta 0.1 --distribution_std 0.2 --initial_population 0.02 --population_size 300 --alpha 0.8 --mutation_rate 0.5  --net ccvae
-python train.py --n_repeats 6 --iters 20 --algorithm v2 --variational_beta 0.1 --distribution_std 0.2 --initial_population 0.02 --population_size 300 --alpha 0.8 --mutation_rate 0.5  --net ccvae
-python train.py --n_repeats 6 --iters 20 --algorithm md --variational_beta 0.1 --distribution_std 0.2 --initial_population 0.02 --population_size 300 --alpha 0.8 --mutation_rate 0.5  --net ccvae
-python train.py --n_repeats 6 --iters 20 --algorithm gd --variational_beta 0.1 --distribution_std 0.2 --initial_population 0.02 --population_size 300 --alpha 0.8 --mutation_rate 0.5  --net ccvae
+python train.py --n_repeats 6 --iters 20 --algorithm v1 --variational_beta 0.1 --distribution_std 0.2 --initial_population 0.002 --population_size 300 --alpha 0.8 --mutation_rate 0.5  --net ccvae
+python train.py --n_repeats 6 --iters 20 --algorithm v2 --variational_beta 0.1 --distribution_std 0.2 --initial_population 0.002 --population_size 300 --alpha 0.8 --mutation_rate 0.5  --net ccvae
+python train.py --n_repeats 6 --iters 20 --algorithm md --variational_beta 0.1 --distribution_std 0.2 --initial_population 0.002 --population_size 300 --alpha 0.8 --mutation_rate 0.5  --net ccvae
+python train.py --n_repeats 6 --iters 20 --algorithm gd --variational_beta 0.1 --distribution_std 0.2 --initial_population 0.002 --population_size 300 --alpha 0.8 --mutation_rate 0.5  --net ccvae
