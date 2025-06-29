d48dc398627a34add8b7709062173d4413315fe5
lucasmurray97 <lucasmurrayh@gmail.com>
2025-06-29 18:18:32 +0200

    Added test file

--- src/algorithms/GA/test.py
+++ src/algorithms/GA/test.py
@@ -1,7 +1,9 @@
-from abstract_ga import Abstract_Genetic_Algorithm
-from variational_GA import Variational_GA_V1
-from variational_GA_v2 import Variational_GA_V2
-from vainilla_ga import Vainilla_GA
+from algorithms import (
+    Abstract_Genetic_Algorithm,
+    Variational_GA_V1,
+    Variational_GA_V2,
+    Vainilla_GA
+)
 import torch
 import sys
 sys.path.append("../../")
@@ -32,15 +34,23 @@ params = {
 net = VAE(params)
 print(net.training)
 net.load_state_dict(torch.load(f'../../weights/homo_2/VAE/sub20x20_latent={latent_dims}_capacity={capacity}_{epochs}_sigmoid={sigmoid}_T1=100_T2=100_lr1={lr1}_lr2=0.0001_lr3=0.0001_normalize=False_weight_decay=0_not_reduced={not_reduced}_variational_beta={variational_beta}_distribution_std={distribution_std}.pth', map_location=torch.device('cpu') ))
-a = Vainilla_GA(net)
-a.initialize_population(alpha=0.01)
-a.train(n_iter=100)
+# try:
+#     a = Vainilla_GA(net, initial_population=0.01)
+#     a.initialize_population(initial_population=0.001)
+#     a.train(n_iter=10)
+# except:
+#     pass
 
-a = Variational_GA_V1(net)
-a.initialize_population(alpha=0.01)
-a.train(n_iter=100)
+try:
+    a = Variational_GA_V1(net, initial_population=0.01)
+    a.initialize_population(initial_population=0.001)
+    a.train(n_iter=10)
+except:
+    pass
 
-a = Variational_GA_V2(net)
-a.initialize_population(alpha=0.01)
-a.train(n_iter=100)
-
+try:
+    a = Variational_GA_V2(net, initial_population=0.01)
+    a.initialize_population(initial_population=0.001)
+    a.train(n_iter=10)
+except:
+    pass
